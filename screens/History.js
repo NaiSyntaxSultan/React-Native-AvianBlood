107ef64d89a4d6a8faa14c26acca34e8009f18ff
Swakiro <151346678+FatornahBk@users.noreply.github.com>
2026-01-17 16:26:05 +0700

    Create History.js & historyItemsCard components

--- screens/History.js
+++ screens/History.js
@@ -1,17 +1,67 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import Navbar from '../components/Navbar'
-import TabBar from '../components/TabBar'
+import React, { useEffect, useState } from "react";
+import { View, FlatList } from "react-native";
+import TabBar from "../components/TabBar";
+import Navbar from "../components/Navbar";
+import HistoryItemCard from "../components/HistoryItemCard";
 
-const History = () => {
+const MOCK_HISTORY = [
+  {
+    id: "1",
+    title: "ตรวจหาเม็ดเลือดขาวของไก่ที่ผิดปกติมัย",
+    datetime: "12/12/2025, 13:00:00 PM",
+    status: "Predict",
+    stain: "Wright",
+    thumbnailUri: "https://picsum.photos/seed/1/80",
+  },
+  {
+    id: "2",
+    title: "ตรวจหาเม็ดเลือดขาวของไก่ที่ผิดปกติมัย",
+    datetime: "12/12/2025, 13:00:00 PM",
+    status: "Pending",
+    stain: "Wright",
+    thumbnailUri: "https://picsum.photos/seed/2/80",
+  },
+  {
+    id: "3",
+    title: "ตรวจหาเม็ดเลือดขาวของไก่ที่ผิดปกติมัย",
+    datetime: "12/12/2025, 13:00:00 PM",
+    status: "Predict",
+    stain: "Giemsa",
+    thumbnailUri: "https://picsum.photos/seed/3/80",
+  },
+  {
+    id: "4",
+    title: "ตรวจหาเม็ดเลือดขาวของไก่ที่ผิดปกติมัย",
+    datetime: "12/12/2025, 13:00:00 PM",
+    status: "Pending",
+    stain: "Giemsa",
+    thumbnailUri: "https://picsum.photos/seed/4/80",
+  },
+];
+
+export default function History() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(MOCK_HISTORY);
+  }, []);
+
   return (
-    <View style={{ flex: 1, position: "relative" }}>
-        <TabBar text={"History"} />
+    <View style={{ flex: 1, backgroundColor: "#fff", position: "relative" }}>
+      <TabBar text={"History"} />
 
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingTop: 18,
+          paddingBottom: 110,
+        }}
+        renderItem={({ item }) => <HistoryItemCard item={item} />}
+      />
 
-        <Navbar />
+      <Navbar />
     </View>
-  )
+  );
 }
-
-export default History
